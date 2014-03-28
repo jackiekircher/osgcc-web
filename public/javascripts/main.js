5f096b2662b06c6037a1dea440d28fd5314ef417
jackie kircher <kircher.jackie@gmail.com>
2014-03-28 00:11:15 -0400

    refactor ajax handling
    the ajax submission was thrown in as a proof-of-concept, but it was just
    sloppy jQuery spaghetti. it's a bit cleaner right now but it's clear
    than I need to establish a javascript file convention in the future.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -6,29 +6,51 @@ var handlers = {
           if(!confirm('Are you sure? This action cannot be undone')) {
             event.preventDefault();
           }
-        }
+        },
+
+  // submit an ajax request rather than an HTTP request to
+  // the button's form's action.
+  ajaxSubmit: function handlersAjaxSubmit(event) {
+                event.preventDefault();
+                var form = $(this).parent();
+
+                $.ajax({
+                  url:      form.attr('action'),
+                  type:     'post',
+                  dataType: 'html',
+                  success:
+                    // this function should be customizable
+                    function(data) {
+                      form.closest("ul").html(data);
+                      loaders.warnIrreversible();
+                    }
+                });
+              }
+};
+
+var loaders = {
+
+  // run all loading functions
+  loadAll:
+    function loadAll() {
+      this.warnIrreversible();
+      this.ajaxSubmit();
+    },
+
+  // add warn event handler to all irreversible actions
+  warnIrreversible: 
+    function loadWarnIrreversible() {
+      $('.irreversible').click(handlers.warn);
+    },
+
+  // bind the ajaxSubmit handler to all ajax form buttons
+  ajaxSubmit:
+    function loadAjaxSubmit() {
+      $('.ajax-submit').click(handlers.ajaxSubmit);
+    }
+
 };
 
 $(document).ready(function() {
-
-  // add warn event handler to all irreversible actions
-  function warnIrreversible() {
-    $('.irreversible').click(handlers.warn);
-  }
-  warnIrreversible();
-
-  $('.ajax-submit').click(function(event) {
-    event.preventDefault();
-    var form = $(this).parent();
-
-    $.ajax({
-      url: form.attr('action'),
-      type: 'post',
-      dataType: 'html',
-      success: function(data) {
-        form.closest("ul").html(data);
-        warnIrreversible();
-      }
-    });
-  });
+  loaders.loadAll();
 });
